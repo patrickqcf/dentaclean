de8192898a8945cf54a6981352f63aa8a69a40c2
adsadilson <adilson.curso@yahoo.com.br>
2023-04-04 19:12:43 -0300

    Refatoração de código e motodo novo existeJornadaTrabalho.

--- odonto-schedulle-service/src/main/java/org/dentaclean/service/JornadaTrabalhoService.java
+++ odonto-schedulle-service/src/main/java/org/dentaclean/service/JornadaTrabalhoService.java
@@ -3,33 +3,48 @@ package org.dentaclean.service;
 import lombok.AllArgsConstructor;
 import org.dentaclean.entity.JornadaTrabalho;
 import org.dentaclean.repository.JornadaTrabalhoRepository;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class JornadaTrabalhoService {
 
-   private JornadaTrabalhoRepository repository;
+    private JornadaTrabalhoRepository repository;
 
-   public JornadaTrabalho salvar(JornadaTrabalho status) {
-      return repository.save(status);
-   }
+    public JornadaTrabalho craete(JornadaTrabalho obj) {
+        existeJornadaTrabalho(obj);
+        return repository.save(obj);
+    }
 
-   public JornadaTrabalho alterar(Long id, JornadaTrabalho status) {
-      JornadaTrabalho stOld = buscarPorId(id);
-      status.setId(stOld.getId());
-      return repository.save(status);
-   }
+    public JornadaTrabalho update(Long id, JornadaTrabalho obj) {
+        findById(id);
+        obj.setId(id);
+        return craete(obj);
+    }
 
-   public JornadaTrabalho buscarPorId(Long id) {
-      Optional<JornadaTrabalho> statusOptional = repository.findById(id);
-      if(statusOptional.isPresent()){
-         return statusOptional.get();
-      }
-      throw new EmptyResultDataAccessException(1);
-   }
+    public JornadaTrabalho findById(Long id) {
+        Optional<JornadaTrabalho> optionalJornadaTrabalho = repository.findById(id);
+        if (optionalJornadaTrabalho.isPresent()) {
+            return optionalJornadaTrabalho.get();
+        }
+        throw new EmptyResultDataAccessException(1);
+    }
+
+    public List<JornadaTrabalho> findByDentistaId(Long dentistaID) {
+        return repository.findAllByDentistaId(dentistaID);
+    }
+
+    private void existeJornadaTrabalho(JornadaTrabalho obj) {
+        Optional<JornadaTrabalho> existed = repository.existeJornadaTrabalho(obj.getDentistaId(), obj.getDiaSemana(),
+                obj.getHoraInicio(), obj.getHoraFim());
+        if (existed.isPresent() && !existed.get().getId().equals(obj.getId())) {
+            throw new DataIntegrityViolationException("Jornada de trabalho já cadastrada");
+        }
+    }
 
 }
