b7cad90a491971cfaaee7fd7541c6b7b80605aa9
adsadilson <adilson.curso@yahoo.com.br>
2023-04-04 19:11:31 -0300

    Aplicação da anotação @NotNull e pegando a mensagem ex.getMessage().

--- odonto-schedulle-service/src/main/java/org/dentaclean/entity/JornadaTrabalho.java
+++ odonto-schedulle-service/src/main/java/org/dentaclean/entity/JornadaTrabalho.java
@@ -3,6 +3,7 @@ package org.dentaclean.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalTime;
 
 @Entity
@@ -14,18 +15,23 @@ public class JornadaTrabalho {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @Column(name = "dentista_id", nullable = false)
     private Long dentistaId;
 
+    @NotNull
     @Column(name = "clinica_id", nullable = false)
     private Long clinicaId;
 
+    @NotNull
     @Column(name = "dia_semana")
-    private int diaSemana;
+    private Integer diaSemana;
 
+    @NotNull
     @Column(name = "hora_inicio", nullable = false)
     private LocalTime horaInicio;
 
+    @NotNull
     @Column(name = "hora_fim", nullable = false)
     private LocalTime horaFim;
 
