00905a0d413dba830253dda8c4c9bf94012ce7dc
adsadilson <adilson.curso@yahoo.com.br>
2023-04-04 19:13:15 -0300

    Criação de dois metodos no repository.

--- odonto-schedulle-service/src/main/java/org/dentaclean/repository/JornadaTrabalhoRepository.java
+++ odonto-schedulle-service/src/main/java/org/dentaclean/repository/JornadaTrabalhoRepository.java
@@ -2,8 +2,26 @@ package org.dentaclean.repository;
 
 import org.dentaclean.entity.JornadaTrabalho;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JornadaTrabalhoRepository extends JpaRepository<JornadaTrabalho, Long> {
+
+    @Query(value = "select jt.* \n" +
+            "\tfrom jornada_trabalho jt \n" +
+            "\t\twhere jt.dentista_id = :dentistaId \n" +
+            "\t\tand jt.dia_semana = :diaSemana\n" +
+            "\t\tand ((:horaInicio >= jt.hora_inicio and :horaInicio <= jt.hora_fim) \n" +
+            "\t\t\tor (:horaFim >= jt.hora_inicio and :horaFim <= jt.hora_fim)\n" +
+            "\t\t)", nativeQuery = true)
+    Optional<JornadaTrabalho> existeJornadaTrabalho(@Param("dentistaId") Long dentistaID, @Param("diaSemana") Integer diaSemana,
+                                                         @Param("horaInicio") LocalTime horaInicio, @Param("horaFim") LocalTime horaFim);
+
+    List<JornadaTrabalho> findAllByDentistaId(Long dentistaId);
 }
