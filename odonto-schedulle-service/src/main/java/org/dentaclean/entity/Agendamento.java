d5141bca30eadcdb047f9f9439bb0ede1fdee6e0
adsadilson <adilson.curso@yahoo.com.br>
2023-04-04 16:50:07 -0300

    Ajuste nos atributos das classes Agendamento, Historico e JornadaTrabalho.

--- odonto-schedulle-service/src/main/java/org/dentaclean/entity/Agendamento.java
+++ odonto-schedulle-service/src/main/java/org/dentaclean/entity/Agendamento.java
@@ -1,10 +1,12 @@
 package org.dentaclean.entity;
 
-import lombok.*;
+import lombok.Data;
+import org.dentaclean.enums.Status;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "agendamento")
@@ -13,19 +15,33 @@ public class Agendamento {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
+
+   @NotNull
    @Column(name = "dentista_id", nullable = false)
    private Long dentistaId;
+
+   @NotNull
    @Column(name = "cliente_id", nullable = false)
    private Long clientId;
+
+   @NotNull
    @Column(name = "data_agendamento", nullable = false)
    private LocalDate dataAgendamento;
+
+   @NotNull
    @Column(name = "hora_inicio", nullable = false)
-   private LocalDateTime horaInicio;
+   private LocalTime horaInicio;
+
+   @NotNull
    @Column(name = "hora_fim", nullable = false)
-   private LocalDateTime horaFim;
-   @ManyToOne
-   @JoinColumn(name = "status_id", nullable = false)
+   private LocalTime horaFim;
+
+
+   @NotNull
+   @Column(nullable = false)
+   @Enumerated(EnumType.STRING)
    private Status status;
+
    @ManyToOne
    @JoinColumn(name = "agendamento_id")
    private Agendamento agendamentoId;
--- odonto-schedulle-service/src/main/java/org/dentaclean/entity/JornadaTrabalho.java
+++ odonto-schedulle-service/src/main/java/org/dentaclean/entity/JornadaTrabalho.java
@@ -3,24 +3,30 @@ package org.dentaclean.entity;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "jornada_trabalho")
 @Data
 public class JornadaTrabalho {
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
-   @Column(name = "dentista_id", nullable = false)
-   private Long dentistaId;
-   @Column(name = "clinica_id", nullable = false)
-   private Long clinicaId;
-   @Column(name = "dia_semana")
-   private int diaSemana;
-   @Column(name = "hora_inicio", nullable = false)
-   private LocalDateTime horaInicio;
-   @Column(name = "hora_fim", nullable = false)
-   private LocalDateTime horaFim;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "dentista_id", nullable = false)
+    private Long dentistaId;
+
+    @Column(name = "clinica_id", nullable = false)
+    private Long clinicaId;
+
+    @Column(name = "dia_semana")
+    private int diaSemana;
+
+    @Column(name = "hora_inicio", nullable = false)
+    private LocalTime horaInicio;
+
+    @Column(name = "hora_fim", nullable = false)
+    private LocalTime horaFim;
 
 }
