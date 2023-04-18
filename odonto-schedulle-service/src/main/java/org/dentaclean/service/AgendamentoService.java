8940216e5b4d7331f4306ac16e2958e3090bcf70
adsadilson <adilson.curso@yahoo.com.br>
2023-04-18 10:25:01 -0300

    Adicionar a inserção dos dados historico nos recurso de agendamento.

--- odonto-schedulle-service/src/main/java/org/dentaclean/service/AgendamentoService.java
+++ odonto-schedulle-service/src/main/java/org/dentaclean/service/AgendamentoService.java
@@ -2,6 +2,7 @@ package org.dentaclean.service;
 
 import lombok.AllArgsConstructor;
 import org.dentaclean.entity.Agendamento;
+import org.dentaclean.entity.Historico;
 import org.dentaclean.enums.Status;
 import org.dentaclean.exceptions.NegocioException;
 import org.dentaclean.repository.AgendamentoRepository;
@@ -10,6 +11,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -17,10 +19,14 @@ import java.util.Optional;
 public class AgendamentoService {
 
     private AgendamentoRepository repository;
+    private HistoricoService historicoService;
 
+    @Transactional
     public Agendamento create(Agendamento obj) {
         existeAgendamento(obj);
-        return repository.save(obj);
+        Agendamento agendamento = repository.save(obj);
+        createHistorico(agendamento);
+        return agendamento;
     }
 
     public Agendamento findById(Long id) {
@@ -31,11 +37,13 @@ public class AgendamentoService {
         throw new EmptyResultDataAccessException(0);
     }
 
+    @Transactional
     public Agendamento cancel(Long id) {
         Agendamento agendamento = findById(id);
         if (agendamento.getStatus() == Status.CRIADO) {
             agendamento.setStatus(Status.CANCELADO);
             agendamento.setAgendamentoId(agendamento);
+            createHistorico(agendamento);
             return repository.save(agendamento);
         }
         throw new NegocioException("Agendamento já foi cancelado ou finalizado anteriormente.");
@@ -55,6 +63,14 @@ public class AgendamentoService {
         if (existed > 0) {
             throw new DataIntegrityViolationException("Agendamento indisponível.");
         }
+    }
+
+    private void createHistorico(Agendamento obj) {
+        Historico historico = new Historico();
+        historico.setAgendamento(obj);
+        historico.setData(LocalDateTime.now());
+        historico.setStatus(obj.getStatus());
+        historicoService.create(historico);
     }
 
 }
