47436e90ed978f23b03692bbbde1c7cfda103d03
adsadilson <adilson.curso@yahoo.com.br>
2023-04-27 14:04:20 -0300

    Ajuste no JornadaTrabalhoService.

--- odonto-schedulle-api/src/main/java/org/dentaclean/config/KafkaAdminConfig.java
+++ odonto-schedulle-api/src/main/java/org/dentaclean/config/KafkaAdminConfig.java
@@ -1,10 +1,8 @@
 package org.dentaclean.config;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.admin.RecordsToDelete;
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.TopicPartition;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +11,7 @@ import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @Configuration
 @RequiredArgsConstructor
@@ -38,11 +37,26 @@ public class KafkaAdminConfig {
     }
 
     public void deleteMessages(String topicName, int partitionIndex, int beforeIndex) {
+
         TopicPartition topicPartition = new TopicPartition(topicName, partitionIndex);
+
         Map<TopicPartition, RecordsToDelete> deleteMap = new HashMap<>();
         deleteMap.put(topicPartition, RecordsToDelete.beforeOffset(beforeIndex));
-        adminClient.deleteRecords(deleteMap);
+
+        DeleteRecordsResult result = adminClient.deleteRecords(deleteMap);
+
+        Map<TopicPartition, KafkaFuture<DeletedRecords>> lowWatermarks = result.lowWatermarks();
+        try {
+            for (Map.Entry<TopicPartition, KafkaFuture<DeletedRecords>> entry : lowWatermarks.entrySet()) {
+                System.out.println(entry.getKey().topic() + " " + entry.getKey().partition() + " " + entry.getValue().get().lowWatermark());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        adminClient.close();
     }
+
+
 
     @Bean
     public NewTopic topicSchedulle() {
